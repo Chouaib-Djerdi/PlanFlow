570921b6873f68bad1f54bc80f6d1448b6f0f050
Chouaib-Djerdi <djerdichouaib@gmail.com>
2024-12-22 18:42:49 +0100

    Projects : Export to PDF

--- planflow_backend/projects/views.py
+++ planflow_backend/projects/views.py
@@ -2,6 +2,12 @@ from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
 from .serializers import ProjectSerializer
 from .models import Project
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from django.http import HttpResponse
+from django.template.loader import get_template
+from xhtml2pdf import pisa
+import os
 
 
 # Create your views here.
@@ -15,3 +21,20 @@ class ProjectViewSet(viewsets.ModelViewSet):
 
     def get_queryset(self):
         return Project.objects.filter(user=self.request.user)
+
+    @action(detail=True, methods=["get"])
+    def export_pdf(self, request, pk=None):
+        project = self.get_object()
+        template = get_template("project_detail_pdf.html")
+        html = template.render({"project": project})
+
+        response = HttpResponse(content_type="application/pdf")
+        response["Content-Disposition"] = (
+            f'attachment; filename="project_{project.id}.pdf"'
+        )
+
+        pisa_status = pisa.CreatePDF(html, dest=response)
+        if pisa_status.err:
+            return Response({"detail": "Error generating PDF"}, status=500)
+
+        return response
